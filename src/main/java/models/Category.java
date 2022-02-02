771b411ae254f7c1ebb7fe99e8d79afbe7adfe85
arkad <mojib.khabireh@gmail.com>
2022-02-02 10:32:55 +0330

    class admin&user add category

--- src/main/java/models/Category.java
+++ src/main/java/models/Category.java
@@ -1,4 +1,42 @@
 package models;
 
 public class Category {
+    private int parentId,id;
+    private String name;
+
+    public Category(int parentId, int id, String name) {
+        this.parentId = parentId;
+        this.id = id;
+        this.name = name;
+    }
+
+    public Category() {
+    }
+
+    public int getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(int parentId) {
+        this.parentId = parentId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
+
+
--- src/main/java/models/Customer.java
+++ src/main/java/models/Customer.java
@@ -1,4 +1,33 @@
 package models;
 
-public class Customer {
+import models.enumoration.TypeUser;
+
+public class Customer extends User{
+    private String address;
+    private long budget;
+
+    public Customer(int id, String fullName, String nationalId, String password, TypeUser typeUser, String address, long budget) {
+        super(id, fullName, nationalId, password, typeUser);
+        this.address = address;
+        this.budget = budget;
+    }
+
+    public Customer() {
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getBudget() {
+        return budget;
+    }
+
+    public void setBudget(long budget) {
+        this.budget = budget;
+    }
 }
