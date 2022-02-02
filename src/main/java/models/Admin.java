c219fa18e8e2aa5efc8be98768c9c582a7a5749d
arkad <mojib.khabireh@gmail.com>
2022-02-02 10:19:07 +0330

    class admin

--- src/main/java/models/Admin.java
+++ src/main/java/models/Admin.java
@@ -1,4 +1,9 @@
 package models;
 
-public class Admin {
+import models.enumoration.TypeUser;
+
+public class Admin extends  User {
+    public Admin(int id, String fullName, String nationalId, String password, TypeUser typeUser) {
+        super(id, fullName, nationalId, password, typeUser);
+    }
 }
