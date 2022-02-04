c7fc4741d688ea823952f6ac91bb7d24158f3643
arkad <mojib.khabireh@gmail.com>
2022-02-04 23:44:44 +0330

    add menu

--- src/main/java/menu.java
+++ src/main/java/menu.java
@@ -1,7 +1,10 @@
-import service.AdminService;
-import service.CustomeServie;
+import models.CustomerBasket;
+import models.Product;
+import service.*;
 
+import javax.swing.*;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Scanner;
 
 public class menu {
@@ -9,16 +12,27 @@ public class menu {
     AdminService adminService = new AdminService();
     CustomeServie customeServie = new CustomeServie();
     Scanner scanner = new Scanner(System.in);
+    LoginService loginService = new LoginService();
+    private int id;
+    boolean istime = false;
+    ProductService productService;
+    CustomerBasketService customerBasketService;
+    SaleProductService saleProductService = new SaleProductService();
 
     public menu() throws SQLException, ClassNotFoundException {
     }
 
     public void menu() throws SQLException {
+
         System.out.println("for sing up insert 1\n " +
                 "for login insert 2");
         select = scanner.nextInt();
         if (select == 1){
             singUp();
+        }
+        else {
+
+
         }
 
 
@@ -30,5 +44,58 @@ public class menu {
             adminService.add();
         } else
             customeServie.add();
+    }
+    private boolean login() throws SQLException {
+        System.out.println("plese insert your id");
+        this.id = scanner.nextInt();
+        System.out.println("plese insert your password");
+        int password=scanner.nextInt();
+        return loginService.isHere(this.id,password);
+    }
+    private void admin(){
+        while (istime){
+            productService = new ProductService();
+        System.out.println("for insert produce insert 1/n" +
+                "for see all customer insert 2");
+        int select = scanner.nextInt();
+        switch (select){
+            case 1:
+                productService.setAdminId(this.id);
+                productService.add();
+                break;
+            case 2:
+                productService.findAll();
+                break;
+            case 3:
+                istime =false;
+        }
+    }}
+
+    private void customer() throws SQLException {
+        while (istime){
+            System.out.println("for sell insert 1\n" +
+                    "for see your basket insert 2\n" +
+                    "for final sale insert 3\n" +
+                    "for delete from basket insert 4\n" +
+                    "for exit insert  or cancell 4");
+            List<CustomerBasket> list = null;
+            int select = scanner.nextInt();
+            customerBasketService.setCustomerId(this.id);
+            switch (select){
+                case 1:
+                    productService.findAll();
+                    customerBasketService.add();
+                    break;
+                case 2:
+                    productService.findAll();
+                   break;
+                case 3:
+                    list = customerBasketService.findAll();
+                    saleProductService.add(list);
+                case 4:
+                case 5:
+                    istime =false;
+            }
+        }
     }
 }
